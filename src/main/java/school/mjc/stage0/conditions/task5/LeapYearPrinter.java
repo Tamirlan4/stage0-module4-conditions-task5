b4485b5d33fe47a7c52b87c890a393dfddc783c4
Tamirlan4 <tamirlan_agubaev@mail.ru>
2022-10-20 11:59:56 +0600

    20 10 2022 11:59

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String result = "";
+        result = year % 4 == 0?
+                "leap":
+                "not leap";
+        switch (year){
+            case 1900:
+                System.out.println("not leap");
+                break;
+            default:
+                System.out.println(result);
+                break;
+        }
     }
 }
